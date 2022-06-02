bddbe371ee899b64ea44c7e0a9cc850c7b9ac1cb
MahiGitH <mahiadamu1@gmail.com>
2022-06-02 16:55:45 -0400

    first commit

---
+++ src/main/java/Hello/Application.java
@@ -0,0 +1,16 @@
+package Hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		hello.GreetingClient greetingClient = context.getBean(hello.GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
+	}
+}
---
+++ src/main/java/Hello/Greeting.java
@@ -0,0 +1,24 @@
+package Hello;
+
+public class Greeting {
+
+    private String message;
+    public Greeting(){
+    }
+
+    Greeting(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+
+    public void setMessage(String message){
+        this.message =message;
+    }
+    @Override
+    public String toString() {
+        return "Greeting {" + "message=' " + message +'\'' + '}';
+    }
+}
